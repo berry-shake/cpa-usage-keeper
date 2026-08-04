afcc7bfb664b969fb30cc0edd5880c0b89a82b24
Lemon <lemon@linux.do>
2026-08-04 10:44:32 -0400

    fix(tests): keep SQLite migrations pure Go

--- internal/repository/migration/test/local_ranking_profile_test.go
+++ internal/repository/migration/test/local_ranking_profile_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"cpa-usage-keeper/internal/repository/migration"
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
--- internal/repository/migration/test/local_ranking_stats_test.go
+++ internal/repository/migration/test/local_ranking_stats_test.go
@@ -7,7 +7,7 @@ import (
 
 	"cpa-usage-keeper/internal/entities"
 	"cpa-usage-keeper/internal/repository/migration"
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
